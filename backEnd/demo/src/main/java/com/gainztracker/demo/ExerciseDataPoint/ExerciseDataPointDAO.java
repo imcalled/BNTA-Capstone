78cdf8939124ae88c4dd46fea1f7b6a6dd64fbfa
jasonsum <jasonsum321@gmail.com>
2021-12-09 10:08:51 +0000

    fixed exercise data point

--- backEnd/demo/src/main/java/com/gainztracker/demo/ExerciseDataPoint/ExerciseDataPointDAO.java
+++ backEnd/demo/src/main/java/com/gainztracker/demo/ExerciseDataPoint/ExerciseDataPointDAO.java
@@ -9,13 +9,7 @@ public interface ExerciseDataPointDAO {
     int postCalisthenics(int exerciseID, LocalDate date, int setsAchieved, int repsAchieved);
     int postWeights(int exerciseID, LocalDate date, int setsAchieved, int repsAchieved, int weightAchieved);
     List<ExerciseDataPoint> getAll();
-
-//     int postCardio(ExerciseDataPoint exerciseDataPoint);
-//     int postCalisthenics(ExerciseDataPoint exerciseDataPoint);
-//     int postWeights(ExerciseDataPoint exerciseDataPoint);
-//     List<ExerciseDataPoint> getAll();
-//     List<ExerciseDataPoint>getDataPointByExerciseID(int id);
-
-//     List<ExerciseDataPoint> getDataPointByExerciseName(String name);
+    List<ExerciseDataPoint>getDataPointByExerciseID(int id);
+    List<ExerciseDataPoint> getDataPointByExerciseName(String name);
 
 }
