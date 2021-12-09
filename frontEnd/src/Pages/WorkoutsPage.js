300e5969ada246a5ff7fae3c4fa27ac272f00031
zahir679 <zahirahmed0512@gmail.com>
2021-12-09 14:44:24 +0000

    workoutpage fetch request

--- frontEnd/src/Pages/WorkoutsPage.js
+++ frontEnd/src/Pages/WorkoutsPage.js
@@ -2,6 +2,16 @@ import React, { useState } from "react";
 import WorkoutsList from "../components/WorkoutsList";
 
 const WorkoutsPage = () => {
+    const [workout, setWorkouts] = useState([]);
+
+  const getAllWorkouts = () => {
+    fetch("http://localhost:8080/api/v1/workout")
+    .then(response => response.json())
+    .then(data => setWorkouts(data))
+  }
+
+  useEffect(getAllWorkouts, [])
+  
     return(
 
         <>
