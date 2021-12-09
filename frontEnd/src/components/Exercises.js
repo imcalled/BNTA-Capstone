f323275ad1caa1908fcf2d839dd047a69b4777cc
queenieyear@gmail.com <you@example.com>
2021-12-09 14:14:50 +0000

    Created containers and components for adding  new workout page

---
+++ frontEnd/src/components/Exercises.js
@@ -0,0 +1,4 @@
+const Exercises = () => {
+
+}
+export default Exercises;
---
+++ frontEnd/src/containers/NewWorkoutContainer.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+const NewWorkoutContainer = () => {
+
+    return (
+        <> 
+        <p>New Workout Page</p>
+        </>
+    )
+}
+export default NewWorkoutContainer;
