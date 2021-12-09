be84ada2e44ef3e46704a493e5547c6891614608
mechanin <86329519+mechanin@users.noreply.github.com>
2021-12-09 12:41:40 +0000

    Completed homepage buttons

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
   <BrowserRouter>
     <div>
-      <h1> HomePage</h1>
+      <h1> GainzTracker</h1>
       <Routes>
         <Route path = "/" element={<HomePage/>} />
         <Route path = "/MyStatsPage" element={<MyStatsPage/>} />
--- frontEnd/src/Pages/HomePage.js
+++ frontEnd/src/Pages/HomePage.js
@@ -1,4 +1,8 @@
 import { useNavigate } from "react-router-dom";
+import "../Styles/HomePage.css";
+import calendarIcon from "../Icons/calendar.png";
+import dumbbellIcon from "../Icons/dumbbell.png";
+import trendIcon from "../Icons/trend.png";
 
 const HomePage = () => {
   let navigate = useNavigate();
@@ -15,12 +19,38 @@ const HomePage = () => {
     navigate("/ProteinTrackerPage");
   };
 
-  return <>
-  This is the homepage
-  <button onClick={goWorkoutsPage}>My Workouts</button>
-  <button onClick={goMyStatsPage}>My Stats</button>
-  <button onClick={goProteinTrackerPage}>Protein Tracker</button>
-  </>;
+  return (
+    <>
+      <h1>My Dashboard</h1>
+      <div className={"container__button_primary"}>
+        <div>
+          <button className={"button__primary"} onClick={goWorkoutsPage}>
+            <img
+              className="icon"
+              src={dumbbellIcon}
+              align="center"
+              alt="Dumbbell"
+            />
+            My Workouts
+          </button>
+        </div>
+
+        <button className={"button__primary"} onClick={goMyStatsPage}>
+          <img className="icon" src={trendIcon} align="center" alt="Dumbbell" />
+          My Stats
+        </button>
+        <button className={"button__primary"} onClick={goProteinTrackerPage}>
+          <img
+            className="icon"
+            src={calendarIcon}
+            align="center"
+            alt="Dumbbell"
+          />
+          Protein Tracker
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
