debb857e16e604cf06a295e44fc38fc7cdec4901
jasonsum <jasonsum321@gmail.com>
2021-12-10 10:10:40 +0000

    Merge branch 'workoutPage' of github.com:imcalled/BNTA-Capstone into workoutPage

--- backEnd/demo/src/main/java/com/gainztracker/demo/configuration/SpringGlobalConfig.java
+++ backEnd/demo/src/main/java/com/gainztracker/demo/configuration/SpringGlobalConfig.java
@@ -1,8 +1,10 @@
 package com.gainztracker.demo.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class SpringGlobalConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
