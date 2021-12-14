ec058e6aac3397a0b7ac2d622f445a8ae05e2381
Jazim Latif <jazimlatif@Jazims-Air.broadband>
2021-12-14 12:28:50 +0000

    getting graph to show up

--- frontEnd/src/components/Graph.js
+++ frontEnd/src/components/Graph.js
@@ -32,3 +32,29 @@ const Graph=({valuesToGraph})=>{
 
 
 export default Graph;
+
+
+    //  if((valuesToGraph.timeAchieved>0 || valuesToGraph.distanceAchieved>0) && valuesToGraph.setsAchieved===0 && valuesToGraph.repsAchieved===0 && valuesToGraph.weightAchieved===0){
+    //     return("Cardio")
+    //     }
+    // else if((valuesToGraph.setsAchieved>0 && valuesToGraph.repsAchieved>0) && valuesToGraph.timeAchieved===0 && valuesToGraph.distanceAchieved===0 && valuesToGraph.weightAchieved===0){
+
+    //     return ("Weights")
+    //     }
+    // else if((valuesToGraph.setsAchieved>0 && valuesToGraph.repsAchieved>0 && valuesToGraph.weightAchieved>0)  && valuesToGraph.timeAchieved===0 && valuesToGraph.distanceAchieved===0){
+        
+    //         return("Calisthenics")
+        
+    //         }
+
+
+            
+    // const renderGraph = () =>{if(valuesToGraph){
+    //     return(valuesToGraph.date)
+    //     }
+    //     else{
+    //     <p>Loading...</p>}
+    // }
+        
+        
+    //     return(renderGraph);
