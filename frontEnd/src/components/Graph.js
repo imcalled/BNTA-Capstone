65059b7e8ab32a6c0acc4584149cc78c375ae025
mechanin <86329519+mechanin@users.noreply.github.com>
2021-12-14 10:25:02 +0000

    Adding graph
    Co-Authored-By: Jazim Latif <74251072+JazimLatif@users.noreply.github.com>

--- frontEnd/src/components/Graph.js
+++ frontEnd/src/components/Graph.js
@@ -1,36 +1,34 @@
-
+import {Line} from 'react-chartjs-2'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const Graph=({valuesToGraph})=>{
 
-
-    // if((valuesToGraph.timeAchieved>0 || valuesToGraph.distanceAchieved>0) && valuesToGraph.setsAchieved===0 && valuesToGraph.repsAchieved===0 && valuesToGraph.weightAchieved===0){
-
-    //     return("Cardio")
-
-    // }
-    // else if((valuesToGraph.setsAchieved>0 && valuesToGraph.repsAchieved>0) && valuesToGraph.timeAchieved===0 && valuesToGraph.distanceAchieved===0 && valuesToGraph.weightAchieved===0){
-
-    //     return("Weights")
-
-    // }
-    // else if((valuesToGraph.setsAchieved>0 && valuesToGraph.repsAchieved>0 && valuesToGraph.weightAchieved>0)  && valuesToGraph.timeAchieved===0 && valuesToGraph.distanceAchieved===0) {
-
-    //     return("Calisthenics")
-
-    // }
-    const renderGraph = () =>{if(valuesToGraph){
-        
-    return(valuesToGraph.date)
-    }
-    else{<p>Loading</p>}}
-
-    
-    return(renderGraph);
+    const [chartData,setChartData]=useState([]);
 
 
+    useEffect(() => {
+        setChartData({
+            labels: valuesToGraph.map((value => value.date)),
+            values: valuesToGraph.map((value => value.weightAchieved))
+        }
+    )},[valuesToGraph]);
 
-
-
+    return(
+        <>
+        <p>Hello</p>
+        <div>
+            
+        <Line
+          data={chartData}
+          options={{
+          }}
+        />
+      </div>
+      </>
+    )
 }
+
+
 
 export default Graph;
