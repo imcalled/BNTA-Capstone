a192f45e11c402a811108421ce3ad76b77f9acf4
mechanin <86329519+mechanin@users.noreply.github.com>
2021-12-15 10:22:13 +0000

    Graph Styling and Axes Labels

--- frontEnd/src/components/Graph.js
+++ frontEnd/src/components/Graph.js
@@ -21,55 +21,87 @@ const Graph=({valuesToGraph, exerciseType})=>{
         }
     )},[valuesToGraph]);
 
+    let yAxisLabel;
     let yAxisData;
+    let unit;
 
-    if(exerciseType==="WEIGHTS"){
-      yAxisData= chartData.valuesWeight
-    }
-    else if (exerciseType==="CALISTHENICS"){
-      yAxisData=chartData.valuesSets*chartData.valuesReps
-    }
-    else if(exerciseType==="CARDIO"){
-      yAxisData = chartData.valuesDistance.map(function(n, i) { return n / chartData.valuesTime[i]; });
+    if (exerciseType === "WEIGHTS") {
+      yAxisData = chartData.valuesWeight;
+      unit = "kg";
+      yAxisLabel = "Weight Lifted"
+    } else if (exerciseType === "CALISTHENICS") {
+      yAxisData = chartData.valuesSets.map(function (n, i) {
+        return n * chartData.valuesReps[i];
+      });
+      unit = "reps";
+      yAxisLabel = "Total Reps"
+    } else if (exerciseType === "CARDIO") {
+      yAxisData = chartData.valuesDistance.map(function (n, i) {
+        return n / chartData.valuesTime[i];
+      });
+      unit = "m/s";
+      yAxisLabel = "Pace"
     }
 
     const data = {
-      datasets: [{
-        label: valuesToGraph.date,
-        data: yAxisData,
-        fill: true,
-        borderColor: 'rgb(75, 192, 192)',
-        tension:0.2
-      }]
-    }
+      datasets: [
+        {
+          label: valuesToGraph.date,
+          data: yAxisData,
+          fill: true,
+          backgroundColor: "transparent",
+          borderColor: "rgba(255,30,0, 0.50)",
+          pointBackgroundColor: "rgba(255, 255, 255, 1)",
+          tension: 0.15,
+          fill: {
+            target: "origin",
+            above: "rgba(255,69,0,0.6)", // Area will be red above the origin
+            below: "rgb(0, 0, 255)", // And blue below the origin
+          },
+        },
+      ],
+    };
 
-    return(
-      
-      valuesToGraph ?
-        <>
+    return valuesToGraph ? (
+      <>
         {console.log(exerciseType)}
         {console.log(data)}
-    
+
         <p>Hello</p>
-        <div>
-        <Line
-          data={data}
-          options={
-            {scales: {
-            x: {labels:chartData.labels},
-            y: {labels:data.datasets.data}
-          }
-         }
-        }
-        />
-      </div>
+        <div id="name">
+          <Line
+            data={data}
+            options={{
+              scales: {
+                x: {
+                  labels: chartData.labels,
+                  title: {
+                    color: "black",
+                    display: true,
+                    text: "Date",
+                  },
+                },
+                y: {
+                  labels: data.datasets.data,
+                  ticks: {
+                    callback: function (value) {
+                      return value + ` ${unit}`;
+                    },
+                  },
+                  title: {
+                    color: "black",
+                    display: true,
+                    text: `${yAxisLabel}`,
+                  },
+                },
+              },
+            }}
+          />
+        </div>
       </>
-      :
-      
+    ) : (
       <p>Loading...</p>
-    
-      
-    )
+    );
 }
  
 
