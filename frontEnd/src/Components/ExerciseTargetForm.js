718dff67fd296301e032ccd88d1c9a31f7e80008
mechanin <86329519+mechanin@users.noreply.github.com>
2021-12-16 15:10:13 +0000

    Edit Workout Container Updates

--- frontEnd/src/Components/ExerciseTargetForm.js
+++ frontEnd/src/Components/ExerciseTargetForm.js
@@ -125,7 +125,7 @@ const ExerciseTargetForm = ({exercise, onAddExerciseTarget}) => {
                     // console.log("cardio")
                     return(
                     <>
-                        <div className="cardio-workout">
+                        <div className="cardio-workout exercise-to-add">
                             <img className="cardio-image" src={cardio}/>    
 
                             <div className="cardio-workout-container">
